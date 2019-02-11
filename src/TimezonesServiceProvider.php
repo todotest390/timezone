02e8dfb8c446fdd80092f8ae6c2e1f178a36819a
Danish <danish@cmswebsiteservices.com>
2019-02-11 14:51:01 +0530

    changes

--- src/TimezonesServiceProvider.php
+++ src/TimezonesServiceProvider.php
@@ -7,21 +7,6 @@ use Illuminate\Support\ServiceProvider;
 class TimezonesServiceProvider extends ServiceProvider
 {
     /**
-     * Register services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        include __DIR__.'/routes/web.php';
-        $this->app->register(Learning\Timezones\TimezonesServiceProvider::class);
-        $this->app->singleton(Learning\Timezones\TimezonesServiceProvider::class, function (Application $app) {
-            return new TimezonesServiceProvider();
-        });
-        $this->app->make('Learning\Timezones\TimezonesController');
-    }
-
-    /**
      * Bootstrap services.
      *
      * @return void
@@ -32,5 +17,19 @@ class TimezonesServiceProvider extends ServiceProvider
         $this->publishes([
         __DIR__.'/views' => base_path('resources/views/learning/timezones'),
     ]);
+    }
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        include __DIR__.'/routes/web.php';
+        $this->app->bind(
+            'Learning\Timezones\TimezonesController'
+        );
+//        $this->app->make('Learning\Timezones\TimezonesController');
     }
 }
